1368a53a1ef58818386712d86a5549b8de75cf8c
rezerwacjeapp <rezerwacje04@gmail.com>
2026-03-28 22:25:04 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -30,8 +30,7 @@ export default defineConfig(({ mode }) => ({
         orientation: "portrait",
         icons: [
           { src: "/pwa-192.png", sizes: "192x192", type: "image/png" },
-          { src: "/pwa-512.png", sizes: "512x512", type: "image/png", purpose: "any maskable" },
-          { src: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+          { src: "/pwa-512.png", sizes: "512x512", type: "image/png" },
         ],
       },
     }),
